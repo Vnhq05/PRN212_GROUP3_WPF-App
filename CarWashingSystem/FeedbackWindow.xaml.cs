d7cf6ce6dd83d5126cb0f5bff77c8ab66d0ac0f2
Dương Văn Thành <duongvanthanh11102005@gmail.com>
2026-07-22 23:40:00 +0700

    staff page

---
+++ CarWashingSystem/FeedbackWindow.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace CarWashingSystem
+{
+    public partial class FeedbackWindow : Window
+    {
+        public FeedbackWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CarWashingSystem/StaffWindow.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace CarWashingSystem
+{
+    public partial class StaffWindow : Window
+    {
+        public StaffWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
